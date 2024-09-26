552625d4f9e6786ad131ece0b30f4c67fd19db1e
Vlad <vladsevruk8@gmail.com>
2024-09-26 18:55:47 +0300

    4

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import math
-X = float(input("Введите объем шара: "))
-r = ((3 * X) / (4 * math.pi)) ** (1/3)
-print("Радиус шара:", r)
+z = float(input("Введите значение z: "))
+y = float(input("Введите значение y: "))
+cos_diff = math.cos(z) - math.cos(y)
+sin_term = (1 + 2 * math.sin(y)) ** (-1/2)
+poly_term = 1 + z / 2 + (z ** 3) / 3 + (z ** 4) / 4
+w = cos_diff * sin_term * poly_term
+print("Значение функции w:", w)
